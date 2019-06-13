40d70227632e952f7f7a1b0c8fdb721324fa0f1b
Colin T.A. Gray <colinta@gmail.com>
2019-06-13 15:56:07 -0400

    fixes date formatter

--- Sources/Networking/DateFormatting.swift
+++ Sources/Networking/DateFormatting.swift
@@ -5,7 +5,7 @@
 let ServerDateFormatter: DateFormatter = {
     let formatter = DateFormatter()
     formatter.locale = Locale(identifier: "en_US")
-    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
     formatter.timeZone = TimeZone(abbreviation: "UTC")
     return formatter
 }()
